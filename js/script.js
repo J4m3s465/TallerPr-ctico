46d20cb28c7b3b6947b6afd06395519ec18aa3e6
J4m3s465 <jamesduran@outlook.com>
2024-03-09 08:17:01 -0500

    Taller

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("formulario").addEventListener("submit", function(event) {
+        event.preventDefault();
+        validarFormulario();
+    });
+});
+
+function validarFormulario() {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let telefono = document.getElementById("telefono").value;
+    let email = document.getElementById("email").value;
+    let grado = document.getElementById("grado").value;
+
+    if (telefono.length !== 10 || isNaN(telefono)) {
+        alert("El número de teléfono debe tener 10 dígitos numéricos.");
+        return;
+    }
+
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Por favor ingrese un email válido.");
+        return;
+    }
+
+    console.log("Nombre:", nombre);
+    console.log("Apellido:", apellido);
+    console.log("Teléfono:", telefono);
+    console.log("Email:", email);
+    console.log("Nivel Educativo de Interés:", grado);
+}
+
 let btnSolicitarInformacion = document.getElementById("btn-solicitar-informacion");
 let modalInformacion = document.getElementById("modal-informacion");
 
@@ -14,32 +47,4 @@ btnCerrarModal.forEach(function(btn) {
         modalInformacion.style.display = 'none';
         modalInformacion.setAttribute('aria-hidden', 'true');
     });
-});
-
-function validarFormulario() {
-    let nombre = document.getElementById("nombre").value;
-    let apellido = document.getElementById("apellido").value;
-    let telefono = document.getElementById("telefono").value;
-    let email = document.getElementById("email").value;
-
-    if (telefono.length !== 10 || isNaN(telefono)) {
-        alert("El número de teléfono debe tener 10 dígitos numéricos.");
-        return false;
-    }
-
-    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        alert("Por favor ingrese un email válido.");
-        return false;
-    }
-
-    return true;
-}
-
-document.getElementById("formulario").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    if (validarFormulario()) {
-        this.submit();
-    }
 });
